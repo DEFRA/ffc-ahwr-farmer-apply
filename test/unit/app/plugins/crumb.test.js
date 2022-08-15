af29221bd92884a46f83b3e1754479ffeeb895b4
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2022-08-15 13:50:38 +0100

    Revert "Add plugins, routing, etc... for ffc-ahwr-farmer-apply to allow for web page builds"
    This reverts commit 015f6ed8380af659cc8e258aac637011e9da700f.

--- test/unit/app/plugins/crumb.test.js
+++
@@ -1,8 +0,0 @@
-const crumbPlugin = require('../../../../app/plugins/crumb')
-
-describe('crumb plugin', () => {
-  test('is correctly configured', () => {
-    expect(crumbPlugin.options.cookieOptions).toHaveProperty('isSecure')
-    expect(crumbPlugin.options.cookieOptions.isSecure).toEqual(false)
-  })
-})
--- test/utils/phase-banner-expect.js
+++
@@ -1,9 +0,0 @@
-function ok ($) {
-  expect($('.govuk-phase-banner').length).toEqual(1)
-  expect($('.govuk-phase-banner').text()).toMatch('beta')
-  expect($('.govuk-phase-banner').text()).toMatch('This is a new service')
-}
-
-module.exports = {
-  ok
-}
