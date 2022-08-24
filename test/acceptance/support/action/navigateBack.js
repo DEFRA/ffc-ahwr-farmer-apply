c240621252bea5e6f2470b8d90fdca709ddb6d11
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-08-24 09:15:43 +0100

    remove acceptance tests

--- test/acceptance/support/action/navigateBack.js
+++
@@ -1,6 +0,0 @@
-/**
- * Navigate back using the browser's back button.
- */
-export default async () => {
-  await browser.back()
-}
--- test/acceptance/support/mock-utils/sendMessage.js
+++
@@ -1,7 +0,0 @@
-const { MessageSender } = require('ffc-messaging')
-
-export default async function sendMessage (config, messageToSend) {
-  const sender = new MessageSender(config)
-  await sender.sendMessage(messageToSend)
-  await sender.closeConnection()
-}
