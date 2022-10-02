09ca98d09e5c59e7b31633d0dfa74aed123c5622
Shivanshu Patel <shivanshuit914@gmail.com>
2022-10-02 23:00:56 +0100

    fix: tests

--- test/integration/narrow/routes/guidance-for-farmers.test.js
+++ test/integration/narrow/routes/guidance-for-farmers.test.js
@@ -46,9 +46,25 @@ describe('Farmer and vet guidance page test', () => {
     expect(res.statusCode).toBe(200)
     const $ = cheerio.load(res.payload)
     expect($('.govuk-heading-xl').text()).toEqual(
-      'Farmers: get a funded annual health and welfare review of your livestock'
+      'Vets: how to carry out an annual health and welfare review of livestock'
     )
-    expect($('title').text()).toEqual(`Guidance for farmers - ${serviceName}`)
+    expect($('title').text()).toEqual(`Guidance for vets - ${serviceName}`)
+    expectPhaseBanner.ok($)
+  })
+  test('GET / route returns 200 when not logged in', async () => {
+    const options = {
+      method: 'GET',
+      url: '/guidance-for-vet-technical'
+    }
+
+    const res = await global.__SERVER__.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    const $ = cheerio.load(res.payload)
+    expect($('.govuk-heading-xl').text()).toEqual(
+      'How to carry out endemic disease and condition testing for an annual health and welfare review of livestock'
+    )
+    expect($('title').text()).toEqual(`Guidance for vets - ${serviceName}`)
     expectPhaseBanner.ok($)
   })
 })
