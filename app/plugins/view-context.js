a97ffc13cc298744c46886ecf4a705d7173b15ea
Shivanshu Patel <shivanshuit914@gmail.com>
2022-10-05 21:13:38 +0100

    feat: add env variable for claim url

--- app/plugins/view-context.js
+++ app/plugins/view-context.js
@@ -1,4 +1,4 @@
-const { serviceName } = require('../config')
+const { serviceName, claimServiceUri } = require('../config')
 
 module.exports = {
   plugin: {
@@ -19,6 +19,9 @@ module.exports = {
           }
           ctx.serviceName = serviceName
           ctx.serviceUrl = serviceUrl
+          ctx.claimServiceUri = claimServiceUri
+
+          console.log(process.env)
 
           response.source.context = ctx
         }
