44a757429f877955b3064ed6ae9db5dfcff9e5b5
Shivanshu Patel <shivanshuit914@gmail.com>
2022-10-18 15:57:12 +0100

    remove: isboom check

--- app/plugins/header.js
+++ app/plugins/header.js
@@ -4,15 +4,9 @@ module.exports = {
     register: (server, options) => {
       server.ext('onPreResponse', (request, h) => {
         const response = request.response
-        if (response.isBoom) {
-          options?.keys?.forEach(x => {
-            response.output.headers[x.key] = x.value
-          })
-        } else {
-          options?.keys?.forEach(x => {
-            response.header(x.key, x.value)
-          })
-        }
+        options?.keys?.forEach(x => {
+          response.header(x.key, x.value)
+        })
         return h.continue
       })
     }
