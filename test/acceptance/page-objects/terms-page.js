293f8fe651a7d88a5e6f6f9bea310d25e1345190
livsey.williams <livsey.williams@defra.onmicrosoft.com>
2022-10-26 11:34:07 +0100

    Update farmer apply scriots submit button element change to accept and send

--- test/acceptance/page-objects/terms-page.js
+++ test/acceptance/page-objects/terms-page.js
@@ -1,8 +1,8 @@
 import Page from './page'
 class TermsPage extends Page {
   get agreeRadioOption () { return browser.$('#terms') }
-  get submitBtn () { return browser.$("//button[contains(.,'Submit application')]") }
-  get rejectBtn () { return browser.$("//a[contains(.,'Reject offer')]") }
+  get submitBtn () { return browser.$("//button[contains(.,'Accept and send')]") }
+  get rejectBtn () { return browser.$("//button[normalize-space()='Reject offer']") }
   async selectAgreeRadioOption () {
     await (await this.agreeRadioOption).scrollIntoView()
     await (await this.agreeRadioOption).click()
