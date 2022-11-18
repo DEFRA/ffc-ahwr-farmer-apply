29e5c72dc9b04cd43f0133485e10833b333987c9
Liam Wilson <liam.wilson@kainos.com>
2022-11-18 15:32:05 +0000

    DEFRA-128125 | Added test for enter crn route

--- test/integration/narrow/routes/register-your-interest.test.js
+++ test/integration/narrow/routes/register-your-interest.test.js
@@ -3,7 +3,7 @@ const expectPhaseBanner = require('../../../utils/phase-banner-expect')
 const { serviceName, urlPrefix } = require('../../../../app/config')
 
 describe('Register your interest page test', () => {
-  test('GET / route returns 200 when not logged in', async () => {
+  test('GET apply/register-your-interest route returns 200 when not logged in', async () => {
     const options = {
       method: 'GET',
       url: `${urlPrefix}/apply/register-your-interest`
@@ -14,6 +14,22 @@ describe('Register your interest page test', () => {
     expect(res.statusCode).toBe(200)
     const $ = cheerio.load(res.payload)
     expect($('.govuk-heading-l').text()).toEqual('Register your interest in a health and welfare review of your livestock')
+
+    expect($('title').text()).toEqual(serviceName)
+    expectPhaseBanner.ok($)
+  })
+
+  test('GET apply/register-your-interest/crn-enter route returns 200 when not logged in', async () => {
+    const options = {
+      method: 'GET',
+      url: `${urlPrefix}/apply/register-your-interest/crn-enter`
+    }
+
+    const res = await global.__SERVER__.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    const $ = cheerio.load(res.payload)
+    expect($('.govuk-heading-l').text()).toEqual('Enter your customer reference number (CRN)')
 
     expect($('title').text()).toEqual(serviceName)
     expectPhaseBanner.ok($)
