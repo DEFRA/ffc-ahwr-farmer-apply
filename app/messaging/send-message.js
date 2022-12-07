701d9903808316ca50a63a705b3aae2e987c5e5a
Liam Wilson <liam.wilson@kainos.com>
2022-12-07 10:06:54 +0000

    Added tests

--- app/messaging/send-message.js
+++ app/messaging/send-message.js
@@ -4,11 +4,7 @@ const createMessage = require('./create-message')
 const sendMessage = async (body, type, config, options) => {
   const message = createMessage(body, type, options)
   const sender = new MessageSender(config)
-  try {
-    await sender.sendMessage(message)
-  } catch (e) {
-    console.log(e)
-  }
+  await sender.sendMessage(message)
   await sender.closeConnection()
 }
 
