657b1978bf43287de7399c531316ed951157eeca
Marcin Mogiela <marcinmo@kainos.com>
2023-02-02 10:01:42 +0100

    DEFRA-FFC-147721: radios refactoring

--- app/routes/models/form-component/select-your-business-radio.js
+++ app/routes/models/form-component/select-your-business-radio.js
@@ -1,33 +1,11 @@
+const radios = require('./radios')
+
 function selectYourBusinessRadioOptions (businesses, legendText, id, previousAnswer, errorText = undefined, options = {}) {
-  const { isPageHeading = true, legendClasses = 'govuk-fieldset__legend--l', inline = false, hintHtml = '' } = options
-  const items = []
-  businesses.forEach(business => {
-    const item = {
-      value: business.sbi,
-      text: `${business.sbi} - ${business.name}`,
-      checked: previousAnswer === business.sbi
-    }
-    items.push(item)
-  })
-  return {
-    radios: {
-      classes: inline ? 'govuk-radios--inline' : undefined,
-      idPrefix: id,
-      name: id,
-      fieldset: {
-        legend: {
-          text: legendText,
-          isPageHeading,
-          classes: legendClasses
-        }
-      },
-      hint: {
-        html: hintHtml
-      },
-      items,
-      ...(errorText ? { errorMessage: { text: errorText } } : {})
-    }
-  }
+  return radios(legendText, id, errorText, options)(businesses.map(business => ({
+    value: business.sbi,
+    text: `${business.sbi} - ${business.name}`,
+    checked: previousAnswer === business.sbi
+  })))
 }
 
 module.exports = {
