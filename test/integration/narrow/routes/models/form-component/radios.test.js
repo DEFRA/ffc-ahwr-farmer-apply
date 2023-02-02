6533a529edceec90cf8baddf930f1a0cf47dfbf5
Marcin Mogiela <marcinmo@kainos.com>
2023-02-02 11:54:29 +0100

    FFC-147721: added test for radios.js

---
+++ test/integration/narrow/routes/models/form-component/radios.test.js
@@ -0,0 +1,58 @@
+const radios = require('../../../../../../app/routes/models/form-component/radios')
+
+describe('radios', () => {
+  test.each([
+    {
+      toString: () => 'Test name',
+      given: {
+        legendText: 'Legend Text',
+        id: 'ID',
+        errorText: 'Error Text',
+        options: {
+          isPageHeading: true,
+          legendClasses: 'govuk-fieldset__legend--l',
+          inline: false
+        }
+      },
+      when: {
+        items: [
+          {
+            name: 'item name'
+          }
+        ]
+      },
+      expect: {
+        radios: {
+          classes: undefined,
+          idPrefix: 'ID',
+          name: 'ID',
+          fieldset: {
+            legend: {
+              text: 'Legend Text',
+              isPageHeading: true,
+              classes: 'govuk-fieldset__legend--l'
+            }
+          },
+          hint: {
+            html: ''
+          },
+          items: [
+            { name: 'item name' }
+          ],
+          errorMessage: {
+            text: 'Error Text'
+          }
+        }
+      }
+    }
+  ])('%s', async (testCase) => {
+    expect(radios(
+      testCase.given.legendText,
+      testCase.given.id,
+      testCase.given.errorText,
+      testCase.given.options
+    )(testCase.when.items)).toEqual(
+      testCase.expect
+    )
+  })
+})
