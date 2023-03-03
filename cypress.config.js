50e70f4ed97ed9b7ec4c16b52ea637d888e313d6
json-hamilton <jason.hamilton@kainos.com>
2023-03-03 14:37:15 +0000

    FFC-157542: fixed tests not running

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('cypress')
 const cucumber = require("cypress-cucumber-preprocessor").default
-require('dotenv').config({ path: `.env${process.env.E2E_ENV}` })
+const URL = require('dotenv').config({ path: `./test/acceptance/.env${process.env.E2E_ENV}` })
 
 module.exports = defineConfig({
   video: true,
@@ -16,7 +16,7 @@ module.exports = defineConfig({
     // You may want to clean this up later by importing these.
     setupNodeEvents (on, config) {
       on("file:preprocessor", cucumber())
-      config.baseUrl = process.env.URL
+      config.baseUrl = URL.parsed.URL
       config.env.NAME = process.env.NAME
       return config
     },
