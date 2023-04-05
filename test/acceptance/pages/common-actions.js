49ca6a120f13a755d2d56ba74503a37683d6d7ba
slimjeo3251 <adekanmiibrahim01@gmail.com>
2023-04-05 16:35:07 +0100

    until

--- test/acceptance/pages/common-actions.js
+++ test/acceptance/pages/common-actions.js
@@ -6,7 +6,6 @@ require("dotenv").config({path:`.env.${process.env.ENV}`})
 class CommonActions {
   async open (path) {
     const url = process.env.TEST_ENVIRONMENT_ROOT_URL + path
-    //console.log('url', url)
     await browser.url(url)
 
   }
@@ -20,7 +19,6 @@ class CommonActions {
     const locator = browser.$(element)
     await locator.setValue(text)
   }
-
   async elementToContainText (element, text) {
     const locator = await browser.$(element)
     expect(await locator.getText()).to.include(text)
@@ -41,5 +39,4 @@ class CommonActions {
     expect(actualUrl).to.include(expectedUrl)
   }
 }
-
 module.exports = CommonActions
