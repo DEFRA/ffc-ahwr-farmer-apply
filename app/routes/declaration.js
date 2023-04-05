acad32993292458871411ea2883d3e045eb523c3
chrissmKainos <christophersm@kainos.com>
2023-04-05 12:15:55 +0100

    171761: Minor change for PR comment

--- app/routes/declaration.js
+++ app/routes/declaration.js
@@ -60,6 +60,8 @@ module.exports = [{
       }
 
       if (!applicationReference) {
+        // TODO: this requires a designed error screen for this scenario
+        // as opposed to the generic screen that this will redirect to.
         console.log('Apply declaration returned a null application reference.')
         throw boom.internal()
       }
