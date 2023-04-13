3e203cae920706393e1889a1267694a8bbc74957
liamwilson-kainos <liam.wilson@kainos.com>
2023-04-13 09:34:36 +0100

    Added further logs

--- app/routes/auth/verify-login.js
+++ app/routes/auth/verify-login.js
@@ -24,7 +24,7 @@ module.exports = [{
         token: Joi.string().uuid().required()
       }),
       failAction: async (request, h, error) => {
-        console.error(error)
+        console.error(`Validation error - ${error.message} for query string - ${request.query}`)
         sendMonitoringEvent(request.yar.id, error.details[0].message, '', getIp(request))
         return h.view('verify-login-failed', {
           backLink: `${config.urlPrefix}/login`
@@ -32,6 +32,8 @@ module.exports = [{
       }
     },
     handler: async (request, h) => {
+      console.log(`Verify Login query parameters - ${request.query}.`)
+      
       const { email, token } = request.query
       const { magiclinkCache } = request.server.app
 
