35be2dbc0849bc60baeff1244b1c586893ea1d6a
json-hamilton <jason.hamilton@kainos.com>
2023-06-05 14:29:04 +0100

    Renamed to ineligible

--- app/event/index.js
+++ app/event/index.js
@@ -1,11 +1,11 @@
 const raiseEvent = require('./raise-event')
 const sendSessionEvent = require('./send-session-event')
 const sendMonitoringEvent = require('./send-monitoring-event')
-const sendExceptionEvent = require('./send-exception-event')
+const sendIneligibilityEvent = require('./send-ineligibility-event')
 
 module.exports = {
   raiseEvent,
   sendSessionEvent,
   sendMonitoringEvent,
-  sendExceptionEvent
+  sendIneligibilityEvent
 }
