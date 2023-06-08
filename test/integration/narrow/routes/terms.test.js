a73d5a1232eb8c9b052b5ce5162aec6c89d2be31
liamwilson-kainos <liam.wilson@kainos.com>
2023-06-08 14:32:41 +0100

    added more test coverage

--- test/integration/narrow/routes/terms.test.js
+++ test/integration/narrow/routes/terms.test.js
@@ -49,6 +49,22 @@ describe('Farmer apply terms and condition page test', () => {
       expect($('#beta-version').text()).toContain('16. Beta 2 timescale')
       expectPhaseBanner.ok($)
     })
+
+    test('GET /terms returns public beta 2 T and C', async () => {
+      const options = {
+        method: 'GET',
+        url: `${urlPrefix}/terms/v2`
+      }
+
+      const res = await global.__SERVER__.inject(options)
+
+      expect(res.statusCode).toBe(200)
+      const $ = cheerio.load(res.payload)
+      expect($('.govuk-heading-l').text()).toEqual('Accept the terms and conditions')
+      expect($('title').text()).toEqual(serviceName)
+      expect($('#beta-version').text()).toContain('16. Beta 2 timescale')
+      expectPhaseBanner.ok($)
+    })
   })
 
   describe('GET - public beta 3 terms and conditions', () => {
