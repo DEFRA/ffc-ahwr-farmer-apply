577db66e358ba6cf12c637be059aaa9a7422c717
liamwilson-kainos <liam.wilson@kainos.com>
2023-06-20 12:00:05 +0100

    Guidance restructrue

--- app/frontend/src/entry.js
+++ app/frontend/src/entry.js
@@ -1,3 +1,7 @@
 import './css/application.scss'
+import './css/document-list.scss'
+import './css/devolved-nations.scss'
+import './css/contents-list.scss'
+import './css/print-link.scss'
 import './js/cookies'
 import './js/handleDuplicateFormSubmissions'
