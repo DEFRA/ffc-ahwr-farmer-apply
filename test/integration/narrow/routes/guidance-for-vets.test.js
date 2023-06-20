d8cf3bf7778df2ebef07d92953189269e144af32
liamwilson-kainos <liam.wilson@kainos.com>
2023-06-20 12:26:32 +0100

    added missing coverage

--- test/integration/narrow/routes/guidance-for-vets.test.js
+++ test/integration/narrow/routes/guidance-for-vets.test.js
@@ -19,4 +19,21 @@ describe('Farmer vet guidance page test', () => {
     expect($('title').text()).toEqual(`Guidance for vets - ${serviceName}`)
     expectPhaseBanner.ok($)
   })
+
+  test('GET / route returns 200 when not logged in', async () => {
+    const options = {
+      method: 'GET',
+      url: `${urlPrefix}/guidance-for-vet-technical`
+    }
+
+    const res = await global.__SERVER__.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    const $ = cheerio.load(res.payload)
+    expect($('.govuk-heading-xl').text()).toEqual(
+      'How to carry out endemic disease and condition testing for an annual health and welfare review of livestock'
+    )
+    expect($('title').text()).toEqual(`Guidance for vets - ${serviceName}`)
+    expectPhaseBanner.ok($)
+  })
 })
