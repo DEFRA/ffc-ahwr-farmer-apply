b0445e88bfbb120f23e4a09ea805be27b9aac0ac
Marcin Mogiela <marcinmo@kainos.com>
2023-06-28 07:35:54 +0200

    fix code

--- app/messaging/send-message.js
+++ app/messaging/send-message.js
@@ -1,5 +1,5 @@
 const createMessage = require('./create-message')
-const createMessageSender = require('./create-message-sender')
+const { createMessageSender } = require('./create-message-sender')
 
 const sendMessage = async (body, type, config, options) => {
   const message = createMessage(body, type, options)
