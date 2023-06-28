82527bc2c9b873c605f65c35d8098ac65080abc6
liamwilson-kainos <liam.wilson@kainos.com>
2023-06-28 13:34:30 +0100

    Added version 1 of T and C's

--- test/integration/narrow/routes/terms.test.js
+++ test/integration/narrow/routes/terms.test.js
@@ -63,6 +63,32 @@ describe('Farmer apply terms and condition page test', () => {
       { queryParam: undefined, continueButtonExpectedLength: 0 },
       { queryParam: '?continue=true', continueButtonExpectedLength: 1 },
       { queryParam: '?continue=false', continueButtonExpectedLength: 0 }
+    ])('GET /terms/v1 returns public beta 1 T and C', async ({ queryParam, continueButtonExpectedLength }) => {
+      const options = {
+        method: 'GET',
+        url: `${urlPrefix}/terms/v1`
+      }
+
+      if (queryParam) {
+        options.url = options.url + queryParam
+      }
+
+      const res = await global.__SERVER__.inject(options)
+
+      expect(res.statusCode).toBe(200)
+      const $ = cheerio.load(res.payload)
+      expect($('.govuk-heading-l').text()).toEqual('Annual health and welfare review of livestock terms and conditions: up to 6 February 2023')
+      expect($('title').text()).toEqual(serviceName)
+      expect($('#beta-version').text()).toContain('16. Phase 1 timescale')
+      expect($('#continueButton').length).toEqual(continueButtonExpectedLength)
+      expectPhaseBanner.ok($)
+    })
+  })
+
+    test.each([
+      { queryParam: undefined, continueButtonExpectedLength: 0 },
+      { queryParam: '?continue=true', continueButtonExpectedLength: 1 },
+      { queryParam: '?continue=false', continueButtonExpectedLength: 0 }
     ])('GET /terms/v2 returns public beta 2 T and C', async ({ queryParam, continueButtonExpectedLength }) => {
       const options = {
         method: 'GET',
