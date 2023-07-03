5c6e87066dc6a602d1bdfd06e18b5f32146f32a3
Marcin Mogiela <marcinmo@kainos.com>
2023-07-03 13:42:57 +0200

    reuse message receiver

--- app/messaging/receive-message.js
+++ app/messaging/receive-message.js
@@ -1,15 +1,17 @@
-const { MessageReceiver } = require('ffc-messaging')
+const cloneDeep = require('rfdc')()
+const { createMessageReceiver } = require('./create-message-receiver')
 
 async function receiveMessage (messageId, config) {
   let result
-  const receiver = new MessageReceiver(config)
-  await receiver.acceptSession(messageId)
-  const messages = await receiver.receiveMessages(1, { maxWaitTimeInMs: 50000 })
+  const receiver = createMessageReceiver(config)
+  const receiverCopy = cloneDeep(receiver)
+  await receiverCopy.acceptSession(messageId)
+  const messages = await receiverCopy.receiveMessages(1, { maxWaitTimeInMs: 50000 })
   if (messages.length) {
     result = messages[0].body
-    await receiver.completeMessage(messages[0])
+    await receiverCopy.completeMessage(messages[0])
   }
-  await receiver.closeConnection()
+  await receiverCopy.closeConnection()
   return result
 }
 
