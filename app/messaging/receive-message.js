4b532afa7c546fdea335b2bb6628d3d240549f6b
Marcin Mogiela <marcinmo@kainos.com>
2023-07-03 16:32:37 +0200

    fix args to acceptSession

--- app/messaging/receive-message.js
+++ app/messaging/receive-message.js
@@ -3,7 +3,7 @@ const { createMessageReceiver } = require('./create-message-receiver')
 async function receiveMessage (messageId, config) {
   let result
   const receiver = createMessageReceiver(config)
-  const sessionReceiver = await receiver.sbClient.acceptSession(messageId)
+  const sessionReceiver = await receiver.sbClient.acceptSession(config.address, messageId)
   const messages = await sessionReceiver.receiveMessages(1, { maxWaitTimeInMs: 50000 })
   if (messages.length) {
     result = messages[0].body
