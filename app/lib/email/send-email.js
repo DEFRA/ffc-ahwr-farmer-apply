0f6db6c2ba46626320b6871ce6e734cc88ef15f6
json-hamilton <jason.hamilton@kainos.com>
2023-07-17 09:18:17 +0100

    fixed logging

--- app/lib/email/send-email.js
+++ app/lib/email/send-email.js
@@ -7,7 +7,7 @@ module.exports = async (templateId, email, options) => {
     console.log(`Successfully sent email with template ID ${templateId} to email ${email}.`)
     return true
   } catch (e) {
-    console.error(`Error occurred during sending email of to ${email} with template ID ${templateId}.`, e)
+    console.error(`Error occurred during sending email of to ${email} with template ID ${templateId}.`, JSON.stringify(e))
     return false
   }
 }
