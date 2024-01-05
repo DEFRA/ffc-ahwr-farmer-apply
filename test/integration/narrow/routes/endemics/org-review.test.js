82292517f82a5dc986adf8e3f31b4509d1d7b8dc
Alex White <alex.a.white@capgemini.com>
2024-01-05 14:25:19 +0000

    Add 404 test on org-review

--- test/integration/narrow/routes/endemics/org-review.test.js
+++ test/integration/narrow/routes/endemics/org-review.test.js
@@ -84,6 +84,23 @@ describe('Org review page test', () => {
       expect(authMock.requestAuthorizationCodeUrl).toBeCalledTimes(1)
       expectPhaseBanner.ok($)
     })
+
+    test('returns 404 when no orgranisation', async () => {
+      session.getFarmerApplyData.mockReturnValue(undefined)
+      const options = {
+        auth,
+        method: 'GET',
+        url
+      }
+  
+      const res = await global.__SERVER__.inject(options)
+  
+      expect(res.statusCode).toBe(404)
+      const $ = cheerio.load(res.payload)
+      expect($('.govuk-heading-l').text()).toEqual('404 - Not Found')
+      expect($('#_404 div p').text()).toEqual('Not Found')
+      expectPhaseBanner.ok($)
+    })
   })
 
   describe(`POST ${url} route`, () => {
