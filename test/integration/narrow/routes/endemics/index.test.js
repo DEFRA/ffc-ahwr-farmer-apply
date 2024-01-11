17c0d8b98a95e12d8f4c0e90cebd9af57e1edd4c
Alex White <alex.a.white@capgemini.com>
2024-01-11 09:58:47 +0000

    Routes test

---
+++ test/integration/narrow/routes/endemics/index.test.js
@@ -0,0 +1,30 @@
+const cheerio = require('cheerio')
+const expectPhaseBanner = require('../../../../utils/phase-banner-expect')
+
+describe('Farmer apply home page test', () => {
+  jest.mock('../../../../../app/config', () => ({
+    ...jest.requireActual('../../../../../app/config'),
+    endemics: {
+      enabled: true
+    }
+  }))
+
+  test('GET / route returns 200 when not logged in', async () => {
+    const options = {
+      method: 'GET',
+      url: '/apply/endemics/start'
+    }
+
+    const res = await global.__SERVER__.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    const $ = cheerio.load(res.payload)
+    expect($('.govuk-heading-l').text()).toEqual(
+      'Apply for funding for livestock health and welfare reviews and follow-ups'
+    )
+    const button = $('.govuk-main-wrapper .govuk-button')
+    expect(button.text()).toMatch('Start now')
+    expect($('title').text()).toEqual('Annual health and welfare review of livestock')
+    expectPhaseBanner.ok($)
+  })
+})
