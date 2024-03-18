7524f7b13437865fb57c2e506fdac8a1c2c11b46
Juan Ortega <juan.ortega-contreras@capgemini.com>
2024-03-18 16:36:31 +0000

    resolve lint

--- app/routes/endemics/claim-guidance.js
+++ app/routes/endemics/claim-guidance.js
@@ -2,7 +2,7 @@ const { endemicsClaimGuidance } = require('../../config/routes')
 const config = require('../../config/index')
 module.exports = [{
   method: 'GET',
-  path: `${config.urlPrefix}/${ endemicsClaimGuidance }`,
+  path: `${config.urlPrefix}/${endemicsClaimGuidance}`,
   options: {
     auth: false,
     handler: async (_, h) => {
