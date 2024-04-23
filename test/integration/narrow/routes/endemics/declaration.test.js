6e0684f343d8d83ebeaa4492ecbae53264282359
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2024-04-23 07:27:49 +0100

    fix declaration failing test

--- test/integration/narrow/routes/endemics/declaration.test.js
+++ test/integration/narrow/routes/endemics/declaration.test.js
@@ -215,7 +215,7 @@ describe('Declaration test', () => {
       const $ = cheerio.load(res.payload)
       expect($('h1').text()).toMatch('Application complete')
       expect($('title').text()).toEqual('Application complete - Get funding to improve animal health and welfare')
-      expect($('h2').text()).toContain('We recommend you arrange a funded follow-up by following these steps')
+      expect($('h2').text()).toContain('recommend you arrange a funded follow-up by following these steps')
       expectPhaseBanner.ok($)
       expect(sessionMock.getFarmerApplyData).toHaveBeenCalledTimes(1)
       expect(sessionMock.setFarmerApplyData).toHaveBeenCalledTimes(2)
