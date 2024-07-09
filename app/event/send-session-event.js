48767bb6a990e5529d74bf80f2888e04b8828b9b
JulieLev <julie.leverington@capgemini.com>
2024-07-09 10:56:58 +0100

    add retry to raiseEvent

--- app/event/send-session-event.js
+++ app/event/send-session-event.js
@@ -1,6 +1,22 @@
 const raiseEvent = require('./raise-event')
+const appInsights = require('applicationinsights')
 
-const sendSessionEvent = (organisation, sessionId, entryKey, key, value, ip, reference = '') => {
+async function retryRaiseEvent (event, maxRetries) {
+  let retries = 0
+  while (retries < maxRetries) {
+    try {
+      await raiseEvent(event)
+      return
+    } catch (error) {
+      console.error(`Apply raiseEvent error ${retries + 1}:`, error)
+      appInsights.defaultClient.trackException({ exception: error })
+      retries++
+    }
+  }
+  console.error(`Apply raiseEvent for event ${event} failed after ${maxRetries} attempts.`)
+}
+
+const sendSessionEvent = async (organisation, sessionId, entryKey, key, value, ip, reference = '') => {
   if (sessionId && organisation) {
     const event = {
       id: sessionId,
@@ -14,7 +30,7 @@ const sendSessionEvent = (organisation, sessionId, entryKey, key, value, ip, ref
       data: { reference, [key]: value },
       ip
     }
-    raiseEvent(event)
+    retryRaiseEvent(event, 2)
   }
 }
 
