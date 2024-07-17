a94bc48ceb78adcdec99ed3a147f01a0bcebb5ba
JulieLev <julie.leverington@capgemini.com>
2024-07-17 16:43:24 +0100

    removed new Error throw

--- app/event/raise-event.js
+++ app/event/raise-event.js
@@ -2,29 +2,33 @@ const { PublishEvent } = require('ffc-ahwr-event-publisher')
 const { eventQueue } = require('../config').mqConfig
 
 const raiseEvent = async (event, status = 'success') => {
-  const eventPublisher = new PublishEvent(eventQueue)
+  try {
+    const eventPublisher = new PublishEvent(eventQueue)
 
-  const eventMessage = {
-    name: event.name,
-    properties: {
-      id: event.id,
-      sbi: event.sbi,
-      cph: event.cph,
-      reference: event.reference,
-      checkpoint: process.env.APPINSIGHTS_CLOUDROLE,
-      status,
-      action: {
-        type: event.type,
-        message: event.message,
-        data: event.data,
-        raisedBy: event.email
+    const eventMessage = {
+      name: event.name,
+      properties: {
+        id: event.id,
+        sbi: event.sbi,
+        cph: event.cph,
+        reference: event.reference,
+        checkpoint: process.env.APPINSIGHTS_CLOUDROLE,
+        status,
+        action: {
+          type: event.type,
+          message: event.message,
+          data: event.data,
+          raisedBy: event.email
+        }
       }
     }
-  }
-  try {
     await eventPublisher.sendEvent(eventMessage)
-  } catch {
-    throw new Error('raiseEvent eventPublisher failed')
+  } catch (err) {
+    if (event.type) {
+      console.error(`Apply raiseEvent for ${event.type} failed`, err)
+    } else {
+      console.error('Apply raiseEvent failed', err)
+    }
   }
 }
 
