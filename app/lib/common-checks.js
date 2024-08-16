d1207ad1dcf3e07be498c8b9169981d4ac319e02
takano <tosin.akano@capgemini.com>
2024-08-16 11:44:55 +0100

    refactor and fix codes for existing user rejected 10months

--- app/lib/common-checks.js
+++ app/lib/common-checks.js
@@ -10,30 +10,18 @@ const calculateMonths = (date, months) => {
 }
 
 const isWithinPeriodFromDate = (date, periodInMonths) => {
-  if (!date || !(date instanceof Date)) {
-    throw new Error('Invalid date provided')
-  }
   const today = new Date()
   const targetDate = calculateMonths(date, periodInMonths)
   return today <= targetDate
 }
 
 const isOfferWithinTenMonths = (offerDate) => {
-  if (!offerDate || !(offerDate instanceof Date)) {
-    throw new Error(`Invalid date provided ${offerDate} `)
-  }
   const periodInMonths = 10
   return isWithinPeriodFromDate(offerDate, periodInMonths)
 }
 
 // check all the conditions
 const isUserOldWorldRejectWithinTenMonths = (applicationData) => {
-  if (!applicationData || !Array.isArray(applicationData)) {
-    throw new Error('Invalid application Data object')
-  }
-  if (applicationData.length < 2) {
-    return { isValid: false, message: 'No old World aggreement' }
-  }
   const dateOfClaim = getValueWhereCondition(
     applicationData,
     'statusId',
@@ -65,17 +53,21 @@ const isUserOldWorldRejectWithinTenMonths = (applicationData) => {
     }
   ]
 
-  console.log(`checks:  ${JSON.stringify(checks)}`)
-
   const failedChecks = checks
     .filter((item) => !item.check)
     .map((item) => item.message)
 
   if (failedChecks.length > 0) {
-    return { isExistingUserRejectedAgreementWithin10months: false, message: failedChecks.join(', ') }
+    return {
+      isExistingUserRejectedAgreementWithin10months: false,
+      message: failedChecks.join(', ')
+    }
   }
 
-  return { isExistingUserRejectedAgreementWithin10months: true }
+  return {
+    isExistingUserRejectedAgreementWithin10months: true,
+    message: 'existingUser, rejected agreement within 10months'
+  }
 }
 
 module.exports = {
--- test/unit/app/lib/common-checks.test.js
+++ test/unit/app/lib/common-checks.test.js
@@ -1,18 +1,16 @@
 const { isUserOldWorldRejectWithinTenMonths } = require('../lib/common-checks')
 
 describe('isUserOldWorldRejectWithinTenMonths', () => {
-
-    test.each([
-        {applicationData: null, expected: { isValid: false, message: 'Invalid application Data object' }},
-        {applicationData: [], expected: { isValid: false, message: 'No old World aggreement' }},
-        {applicationData: [{ dateOfClaim: 'invalid date' }, { dateOfClaim: 'invalid date' }], expected: { isValid: false, message: 'Invalid date provided invalid date' }},
-        {applicationData: [{ dateOfClaim: new Date('2021-01-01') }, { dateOfClaim: new Date('2021-01-01') }], expected: { isValid: false, message: 'Offer is not within 10 months' }},
-        {applicationData: [{ type: 'VV' }, { type: 'VV' }], expected: { isValid: false, message: 'existing user' }},
-        {applicationData: [{ statusId: 2 }, { statusId: 2 }], expected: { isValid: false, message: 'Offer is rejected' }},
-        {applicationData: [{ dateOfClaim: new Date(), type: 'VV', statusId: 1 }, { dateOfClaim: new Date(), type: 'VV', statusId: 1 }], expected: { isValid: true }}
-    ])('returns $expected when $applicationData is passed', ({ applicationData, expected }) => {
-        const result = isUserOldWorldRejectWithinTenMonths(applicationData)
-        expect(result).toEqual(expected)
-    })
-
+  test.each([
+    { applicationData: null, expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'Invalid application Data object' } },
+    { applicationData: [], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'No old World aggreement' } },
+    { applicationData: [{ dateOfClaim: 'invalid date' }, { dateOfClaim: 'invalid date' }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'Invalid date provided invalid date' } },
+    { applicationData: [{ dateOfClaim: new Date('2021-01-01') }, { dateOfClaim: new Date('2021-01-01') }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'Offer is not within 10 months' } },
+    { applicationData: [{ type: 'VV' }, { type: 'VV' }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'existing user' } },
+    { applicationData: [{ statusId: 2 }, { statusId: 2 }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'Offer is rejected' } },
+    { applicationData: [{ dateOfClaim: new Date(), type: 'VV', statusId: 1 }, { dateOfClaim: new Date(), type: 'VV', statusId: 1 }], expected: { isExistingUserRejectedAgreementWithin10months: true } }
+  ])('returns $expected when $applicationData is passed', ({ applicationData, expected }) => {
+    const result = isUserOldWorldRejectWithinTenMonths(applicationData)
+    expect(result).toEqual(expected)
+  })
 })
