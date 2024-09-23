e2c0c89bba93b06d8bce87e0e8e3ddfc765ee5e6
takano <tosin.akano@capgemini.com>
2024-09-23 08:43:44 +0100

    add missing test

--- test/unit/app/lib/common-checks.test.js
+++ test/unit/app/lib/common-checks.test.js
@@ -1,4 +1,4 @@
-const { isUserOldWorldRejectWithinTenMonths } = require('../../../../app/lib/common-checks')
+const { isUserOldWorldRejectWithinTenMonths, isUserOldWorldReadyToPayWithinTenMonths } = require('../../../../app/lib/common-checks')
 
 describe('isUserOldWorldRejectWithinTenMonths', () => {
   test.each([
@@ -10,6 +10,20 @@ describe('isUserOldWorldRejectWithinTenMonths', () => {
     { applicationData: [{ dateOfClaim: '2024-01-01', type: 'VV', statusId: 10 }, { dateOfClaim: new Date(), type: 'VV', statusId: 10 }], expected: expect.objectContaining({ isExistingUserRejectedAgreementWithin10months: false }) }
   ])('returns $expected when $applicationData is passed', ({ applicationData, expected }) => {
     const result = isUserOldWorldRejectWithinTenMonths(applicationData)
+    expect(result).toEqual(expected)
+  })
+})
+
+describe('isUserOldWorldReadyToPayWithinTenMonths', () => {
+  test.each([
+    { applicationData: [], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'new user, Offer is not Ready To Pay' } },
+    { applicationData: [{ dateOfClaim: 'invalid date' }, { dateOfClaim: 'invalid date' }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'new user, Offer is not Ready To Pay' } },
+    { applicationData: [{ dateOfClaim: new Date('2021-01-01') }, { dateOfClaim: new Date('2021-01-01') }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'new user, Offer is not Ready To Pay' } },
+    { applicationData: [{ type: 'VV' }, { type: 'EE' }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'Offer is not Ready To Pay' } },
+    { applicationData: [{ statusId: 2 }, { statusId: 2 }], expected: { isExistingUserRejectedAgreementWithin10months: false, message: 'new user, Offer is not Ready To Pay' } },
+    { applicationData: [{ dateOfClaim: '2024-01-01', type: 'VV', statusId: 9 }, { dateOfClaim: new Date(), type: 'VV', statusId: 9 }], expected: expect.objectContaining({ isExistingUserRejectedAgreementWithin10months: false }) }
+  ])('returns $expected when $applicationData is passed', ({ applicationData, expected }) => {
+    const result = isUserOldWorldReadyToPayWithinTenMonths(applicationData)
     expect(result).toEqual(expected)
   })
 })
