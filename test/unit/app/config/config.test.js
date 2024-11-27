481189c77259496ab9e6871e2e327f36d5701846
Matt O'Donoghue <mattyod@gmail.com>
2024-11-27 18:04:34 +0000

    bump

--- test/unit/app/config/config.test.js
+++ test/unit/app/config/config.test.js
@@ -13,8 +13,7 @@ test('uses defaults', () => {
     .toBe(6379)
   expect(config.port)
     .toBe(3000)
-  expect(config.urlPrefix)
-    .toBe('/apply')
+  expect(config.urlPrefix).toBe('/apply')
 })
 
 test('throws an error for invalid config', () => {
