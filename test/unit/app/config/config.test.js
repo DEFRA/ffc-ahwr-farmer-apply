e777cfddbd45580a17a343980f59782e40d6c4d1
Matt O'Donoghue <mattyod@gmail.com>
2024-11-27 17:49:53 +0000

    more coverage

--- test/unit/app/config/config.test.js
+++ test/unit/app/config/config.test.js
@@ -1,3 +1,22 @@
+test('uses defaults', () => {
+  jest.resetModules()
+  delete process.env.REDIS_HOSTNAME
+  delete process.env.REDIS_PORT
+  delete process.env.PORT
+  delete process.env.URL_PREFIX
+
+  const config = require('../../../../app/config')
+
+  expect(config.cache.options.host)
+    .toBe('redis-hostname.default')
+  expect(config.cache.options.port)
+    .toBe(6379)
+  expect(config.port)
+    .toBe(3000)
+  expect(config.urlPrefix)
+    .toBe('/apply')
+})
+
 test('throws an error for invalid config', () => {
   jest.resetModules()
   delete process.env.COOKIE_PASSWORD
