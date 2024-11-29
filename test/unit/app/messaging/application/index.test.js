5d9cb0144e6503608daec91fcd1636c78aed642c
Matt O'Donoghue <mattyod@gmail.com>
2024-11-29 17:10:42 +0000

    chore(AHWR): remove unused getApplication method

--- test/unit/app/messaging/application/index.test.js
+++ test/unit/app/messaging/application/index.test.js
@@ -1,5 +1,5 @@
-const { getApplication, sendApplication } = require('../../../../../app/messaging/application')
-const { applicationRequestQueue, applicationResponseQueue, fetchApplicationRequestMsgType } = require('../../../../../app/config').mqConfig
+const { sendApplication } = require('../../../../../app/messaging/application')
+const { applicationResponseQueue } = require('../../../../../app/config').mqConfig
 
 jest.mock('../../../../../app/messaging')
 const { receiveMessage, sendMessage } = require('../../../../../app/messaging')
@@ -9,20 +9,6 @@ describe('application messaging tests', () => {
 
   beforeEach(() => {
     jest.resetAllMocks()
-  })
-
-  test('getApplication sends and receives message', async () => {
-    const reference = 'VV-1234-5678'
-    const receiveMessageRes = { id: 1 }
-    receiveMessage.mockResolvedValue(receiveMessageRes)
-
-    const message = await getApplication(reference, sessionId)
-
-    expect(message).toEqual(receiveMessageRes)
-    expect(receiveMessage).toHaveBeenCalledTimes(1)
-    expect(receiveMessage).toHaveBeenCalledWith(sessionId, applicationResponseQueue)
-    expect(sendMessage).toHaveBeenCalledTimes(1)
-    expect(sendMessage).toHaveBeenCalledWith({ applicationReference: reference }, fetchApplicationRequestMsgType, applicationRequestQueue, { sessionId })
   })
 
   test('sendApplication returns undefined on non submitted state', async () => {
