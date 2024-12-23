24856abf75dae97e0cbc3f113e2df008ec141e90
Rob Catton <robmcatton@gmail.com>
2024-12-23 13:57:35 +0000

    Added profanity scan on randomly generated IDs

--- app/lib/create-temp-reference.js
+++ app/lib/create-temp-reference.js
@@ -1,15 +1,19 @@
 const { randomInt } = require('node:crypto')
+const { profanity } = require('@2toad/profanity')
 
-/**
- * Generate unique reference number, prefixed by TEMP
- * ex. TEMP-Z4F1-F2PC
- * @returns string
- */
-function generateRandomID () {
+const containsSwearWord = (input) => {
+  return profanity.exists(input)
+}
+
+const generateRandomID = () => {
   const charset = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789'
   const id = Array.from({ length: 8 }, () => charset.charAt(randomInt(0, charset.length))).join('')
   const firstFour = id.slice(0, 4)
   const secondFour = id.slice(4)
+
+  if (containsSwearWord(firstFour) || containsSwearWord(secondFour) || containsSwearWord(`${firstFour}${secondFour}`)) {
+    return generateRandomID()
+  }
 
   return `TEMP-${firstFour}-${secondFour}`
 }
