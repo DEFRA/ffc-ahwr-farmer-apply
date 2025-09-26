bdb9f70f38f46d3b16bb1d9818edb8ae2e48ba47
Rob Catton <robmcatton@gmail.com>
2025-09-26 14:47:11 +0100

    PR comment

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -7,5 +7,5 @@ export const getLatestApplication = (latestApplicationsForSbi) => {
 export const isWithin10Months = (d) => {
   const date = new Date(d);
   const datePlus10Months = date.setMonth(date.getMonth() + 10);
-  return datePlus10Months >= new Date().valueOf();
+  return datePlus10Months >= Date.now();
 };
